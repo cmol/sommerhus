ae92eb72d9d7c5e87d87c98e8a1d0eeede31f609
Claus Michael Oest Lensbøl <cmol@lensboel.dk>
2009-11-04 16:19:25 +0100

    added mysql_real_escape_string to clean.php

--- req/clean.php
+++ req/clean.php
@@ -5,6 +5,7 @@ function clean($txt)
 	$find		= array("æ", "ø", "å", "Æ", "Ø", "Å", "\&quot;", "\&#039;");
 	$replace	= array("&aelig;", "&oslash;", "&aring;", "&AElig;", "&Oslash;", "&Aring;", "&quot;", "&#039;");
 	$txt = str_replace($find, $replace, $txt);
+	$txt = mysql_real_escape_string($txt);
 	return $txt;
 }
 ?>
